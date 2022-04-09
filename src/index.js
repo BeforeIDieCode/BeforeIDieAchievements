a5ced3ecbd317977c5107bb6facef1ab8c2d60f8
Banana021s <arash.k021@yahoo.com>
2022-04-10 00:55:31 +0430

    create some initial files and classes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div>prepare the react project to start</div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<h1>prepare react project to code</h1>);
