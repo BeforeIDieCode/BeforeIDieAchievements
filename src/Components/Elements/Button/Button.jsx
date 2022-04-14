aecbef7fb040974118542b707c211fc0b7e2374d
Banana021s <arash.k021@yahoo.com>
2022-04-15 01:00:31 +0430

    validate props of Button component

--- src/Components/Elements/Button/Button.jsx
+++ src/Components/Elements/Button/Button.jsx
@@ -1,5 +1,10 @@
+// import styles of this component
 import styles from "./Button.module.css"
 
+// import react package
+import PropTypes from 'prop-types'
+
+// Button component
 const Button = ({ children, theme, onClick }) => (
     <button 
         className={`${styles.button} ${styles[theme]}`}
@@ -8,5 +13,11 @@ const Button = ({ children, theme, onClick }) => (
         {children}
     </button>
 )
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+    theme: PropTypes.string.isRequired,
+    onClick: PropTypes.func
+}
 
 export default Button
