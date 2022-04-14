a29ee3a4dae31a8a209f3836120f1300ebdeea73
Banana021s <arash.k021@yahoo.com>
2022-04-15 00:56:42 +0430

    validate props of ContainerCard component

--- src/Components/ContainerCard/ContainerCard.jsx
+++ src/Components/ContainerCard/ContainerCard.jsx
@@ -1,7 +1,17 @@
+// import styles of this component
 import styles from "./ContainerCard.module.css"
 
+// import react package
+import PropTypes from 'prop-types'
+
+// ContainerCard component
 const ContainerCard = ({ children, className }) => (
     <div className={`${styles.container} ${className}`}>{ children }</div>
 )
+
+ContainerCard.propTypes = {
+    children: PropTypes.node.isRequired,
+    className: PropTypes.string
+}
 
 export default ContainerCard
