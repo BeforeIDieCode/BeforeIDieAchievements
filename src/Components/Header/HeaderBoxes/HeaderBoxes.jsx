6277c4c840a9b70a4cc8392acec5a780bdb9ffcb
Banana021s <arash.k021@yahoo.com>
2022-04-15 01:02:58 +0430

    validate props of HeaderBoxes component

--- src/Components/Header/HeaderBoxes/HeaderBoxes.jsx
+++ src/Components/Header/HeaderBoxes/HeaderBoxes.jsx
@@ -4,6 +4,10 @@ import styles from "./HeaderBoxes.module.css"
 // import other components
 import HeaderBox from "../HeaderBox/HeaderBox"
 
+// import react package
+import PropTypes from 'prop-types'
+
+// HeaderBoxes component
 const HeaderBoxes = ({ titles_numbers }) => (
     <div className={`${styles["header-wrap-boxes"]} flex`}>
         {titles_numbers.map((title_number, idx) => (
@@ -16,5 +20,9 @@ const HeaderBoxes = ({ titles_numbers }) => (
         ))}
     </div>
 )
+
+HeaderBoxes.propTypes = {
+    titles_numbers: PropTypes.array.isRequired
+}
 
 export default HeaderBoxes
