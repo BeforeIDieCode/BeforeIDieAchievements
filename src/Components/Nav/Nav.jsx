b10dd253438bd7b46d6ee5656d7b7b0a838f89a3
Banana021s <arash.k021@yahoo.com>
2022-04-22 01:35:37 +0430

    responsive the Header

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,6 +4,9 @@ import styles from "./Nav.module.css"
 // import other components
 import Button from "../Elements/Button/Button"
 
+// import other react pkg to use
+import { HambergerMenu } from "iconsax-react"
+
 // Nav component
 const Nav = () => {
   return (
@@ -22,16 +25,21 @@ const Nav = () => {
             <li className={styles["nav-item"]}>
                 <a href="" className={styles["nav-link"]}>Artists</a>
             </li>
-            <li className={styles["nav-item"]}>
+            <li className={`${styles["nav-item"]} ${styles["d-none-1100"]}`}>
                 <a href="" className={styles["nav-link"]}>Explore</a>
             </li>
-            <li className={styles["nav-item"]}>
+            <li className={`${styles["nav-item"]} ${styles["d-none-1100"]}`}>
                 <a href="" className={styles["nav-link"]}>Blog</a>
             </li>
         </ul>
         <div className={`flex ${styles["navbar-buttons"]}`}>
             <Button theme="transparent">Login</Button>
             <Button theme="matrix">Sign up</Button>
+        </div>
+        <div className={styles["navbar-responsive-menu"]}>
+            <Button theme="transparent">
+                <HambergerMenu size="32" color="var(--white-100)"/>
+            </Button>
         </div>
     </nav>
   )
