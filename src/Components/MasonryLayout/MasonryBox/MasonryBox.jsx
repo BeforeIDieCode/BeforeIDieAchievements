8b821c595e6ddff893aff6f018a84c1eca06d173
XanderRubio <clemens.alex7@gmail.com>
2023-08-09 19:44:35 +0200

    Formatting of avatar image, clickable, and adding GitHub new window link

--- src/Components/MasonryLayout/MasonryBox/MasonryBox.jsx
+++ src/Components/MasonryLayout/MasonryBox/MasonryBox.jsx
@@ -1,27 +1,30 @@
-// import styles of this component
 import styles from "./MasonryBox.module.css"
 import { PropTypes } from 'prop-types';
 
 // MasonryBox component
 const MasonryBox = ({ wallSrc, userProf, userName, userJob, githubUrl }) => {
   const handleUserProfClick = () => {
-    window.location.href = githubUrl;
-  }
+    if (githubUrl) {
+      window.open(githubUrl, '_blank');
+    }
+  };
 
   return (
     <div className={styles["my-masonry"]}>
-        <img src={wallSrc} style={{ width: "100%" }} alt="" />
-        <div className={`${styles["my-masnry-description"]} flex`}>
-          <div className={`${styles["my-masnry-user-box"]} flex align-items-center`}>
-            <div className={styles["my-masnry-user-prof"]} onClick={handleUserProfClick}>
-              <img src={userProf} alt="" />
-            </div>
-            <div className={`${styles["my-masnry-user-prof-desc"]} flex flex-column`}>
-              <h1>{userName}</h1>
-              <h3>{userJob}</h3>
-            </div>
+      <img src={wallSrc} style={{ width: "100%" }} alt="" />
+      <div className={`${styles["my-masnry-description"]} flex`}>
+        <div className={`${styles["my-masnry-user-box"]} flex align-items-center`}>
+          <div className={styles["my-masnry-user-prof"]}>
+            <a href={githubUrl} target="_blank" rel="noopener noreferrer" onClick={handleUserProfClick}>
+              <img src={userProf} alt="" className={styles["clickable"]} />
+            </a>
+          </div>
+          <div className={`${styles["my-masnry-user-prof-desc"]} flex flex-column`}>
+            <h1>{userName}</h1>
+            <h3>{userJob}</h3>
           </div>
         </div>
+      </div>
     </div>
   )
 }
@@ -32,7 +35,7 @@ MasonryBox.propTypes = {
   userProf: PropTypes.string.isRequired,
   userName: PropTypes.string.isRequired,
   userJob: PropTypes.string.isRequired,
-  githubUrl: PropTypes.string.isRequired,
+  githubUrl: PropTypes.string, // Add the prop type for the GitHub URL
 }
 
 export default MasonryBox
