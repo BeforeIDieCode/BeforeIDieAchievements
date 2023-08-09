296211d723390bedee9766b1108ab9c100d4f964
XanderRubio <clemens.alex7@gmail.com>
2023-08-09 18:47:05 +0200

    Updating App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import { useState } from "react";
-
-// import styles of this component
-import styles from "./App.module.css"
-
-// import other components to use
+import styles from "./App.module.css";
 import Header from './Components/Header/Header';
 import MasonryLayout from './Components/MasonryLayout/MasonryLayout';
 import ContainerCard from './Components/ContainerCard/ContainerCard';
+import images from "./Jsons/Images.json";
 
-// import json file
-import images from "./Jsons/Images.json"
-
-// App component
 const App = () => {
   const [categoryImage, setCategoryImage] = useState(images);
 
+  const handleCategoryChange = (newCategory) => {
+    setCategoryImage(images.filter((image) => image.category === newCategory));
+  };
+
   return (
     <>
-      <Header />
+      <Header onCategoryChange={handleCategoryChange} />
       <div className="flex justify-content-center" style={{ marginTop: "50px", padding: '50px' }}>
         <ContainerCard>
           <div className={`${styles["gallery-setting"]} flex justify-content-between align-items-center`}></div>
@@ -26,6 +23,6 @@ const App = () => {
       </div>
     </>
   );
-}
+};
 
 export default App;
