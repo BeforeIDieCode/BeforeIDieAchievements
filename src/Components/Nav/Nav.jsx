b07408db9380bba026873e6a40ce37ca5d0e2cc5
XanderRubio <clemens.alex7@gmail.com>
2023-08-09 21:09:12 +0200

    Updating style for GitHub button

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,22 +1,47 @@
-import styles from "./Nav.module.css";
+// Nav.jsx
 
-import Button from "../Elements/Button/Button";
+import styles from './Nav.module.css';
+import Button from '../Elements/Button/Button';
 
 const Nav = () => {
+
   return (
     <nav className={`${styles.nav} flex align-items-center`}>
-      <img src="./img/Logo.gif" alt="Before I Die Logo" className="img-fluid" />
 
-      <div className={styles["navbar-buttons"]}>
+      <img
+        src="./img/Logo.gif"
+        alt="Before I Die Logo"
+        className="img-fluid"
+      />
+
+      <div className={styles['navbar-buttons']}>
+
         <Button theme="matrix">
-          <a href="https://github.com/BeforeIDieCode/BeforeIDieAchievements" style={{ color: 'black', display: 'flex', alignItems: 'center' }} target="_blank">
-          <img src="./img/GitHub Logo.png" alt="GitHub Logo" className={styles["github-logo"]} style={{ width: '40px', height: '40px' }} />
+
+          <a
+            href="https://github.com/BeforeIDieCode/BeforeIDieAchievements"
+            className={styles['github-link']}
+            target="_blank"
+          >
+
+            <img
+              src="./img/GitHub Logo.png"
+              alt="GitHub Logo"
+              className={styles['github-logo']}
+              style={{ width: '40px', height: '40px' }}
+            />
+
             GitHub
+
           </a>
+
         </Button>
+
       </div>
+
     </nav>
   );
-}
+
+};
 
 export default Nav;
