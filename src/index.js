d8a1cee09f82344eb56df7a00bc075a6166de3de
Negar <nasirinegar13@gmail.com>
2023-09-05 10:49:23 +0100

    imported config file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import "./index.css"
-
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "./i18nextConfig.js";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
