026258e9ea7dabcaff63cf0a8dbea0e5960d16f4
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-10-11 21:35:40 -0300

    feat(#185): ✨ refactored syntax of ternary if
    Add bid-text class

--- src/Components/MasonryLayout/MasonryBox/MasonryBox.jsx
+++ src/Components/MasonryLayout/MasonryBox/MasonryBox.jsx
@@ -48,9 +48,11 @@ const MasonryBox = ({
   const RTL_LANGUAGES = ["ar", "he", "az", "dv", "ku", "fa", "ur"];
   const BIDText = (
     <span className={styles.boldText}>
+      <div className={styles["bid-text"]}>
        {RTL_LANGUAGES.includes(languages[ipObj.ipInfo?.country_code])
           ? `...${t("BEFORE-I-DIE")}`
           : `${t("BEFORE-I-DIE")}...`}
+      </div>
       <br />
     </span>
   );
