3fab0aaf657cc08c13a410e8c3f91970e4edd093
Sumit Upadhyay <skumarupadhyay1@gmail.com>
2023-10-12 20:43:41 +0530

    [Feature Request] Add footer to other two pages -- Task Completed

--- src/pages/Contributors.jsx
+++ src/pages/Contributors.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import contributors from '../Jsons/Contributors.json'; 
 import ContributorsGrid from '../Components/ContributorsGrid/ContributorsGrid';
+import Footer from '../Components/Footer/Footer';
 
 const Contributors = () => {
   return (
     <div>
      <ContributorsGrid contributors={contributors}/>
+     <Footer/>
     </div>
   )
 }
